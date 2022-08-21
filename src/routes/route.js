15b787ca3ff70f4b61715702a64d83e9f8dbb06c
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-08-21 20:05:18 +0530

    12-agu-22 pending assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,39 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let person =[
+    {
+        name: "pk",
+        age: 10,
+        votingstatus: false
+    },
+    {
+        name: "sk",
+        age: 20,
+        votingstatus: false
+    },
+    {
+        name: "sp",
+        age: 40,
+        votingstatus: false
+    },
+    {
+        name: "ho",
+        age: 70,
+        votingstatus: false
+    },
+    {
+        name: "oo",
+        age: 5,
+        votingstatus: false
+    },
+]
+router.post("/voting", function (req, res) {
+    inputAge=req.query.votingAge;
+    person.map(per=>(per.age>inputAge)?per.votingstatus=true:per.votingstatus);
+    let final=person.filter(pson=>{if(pson.votingstatus==true){return pson.name}});
+    res.send({data:final});
+});
+
+
 module.exports = router;
